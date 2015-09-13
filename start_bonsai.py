f7deaccb42e199d03a8f9201bcf9d5cfc77726dc
xanxys <xanxys@gmail.com>
2015-09-13 21:21:34 +0900

    Add --fake option for very quick iteration.

--- start_bonsai.py
+++ start_bonsai.py
@@ -17,7 +17,7 @@ def get_container_tag(container_name):
     return "gcr.io/%s/%s:%s" % (project_name, container_name, label)
 
 def create_containers(container_name):
-    print("Creating container")
+    print("Creating container %s" % container_name)
     # Without clean, bazel somehow won't update
     subprocess.call(["bazel", "clean"], cwd="./src")
     subprocess.call(["bazel", "build", "frontend:server"], cwd="./src")
@@ -49,17 +49,44 @@ def deploy_containers_gke(container_name):
         '--update-period=10s',
         '--image=%s' % container_name])
 
+def run_fake_server():
+    fake_port = 7000
+    fallback_url = "http://localhost:8000/"
+    print("Running fake server at http://localhost:%d/ with fallback %s" % (
+        fake_port, fallback_url))
+
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser("Launch bonsai service")
-    parser.add_argument('--local', default=False, action='store_const',
-        const=True, help="Run locally (default: run remotely)")
+    parser = argparse.ArgumentParser("Launch bonsai service or fake server")
+    parser.add_argument("--fake",
+        default=False, action='store_const', const=True,
+        help="""
+        Run very lightweight fake python server, which is useful for client dev.
+        Files are served directly (no container) from working tree, and all
+        unknown requests are redirected to localhost:8000, which can be launched
+        by --local.
+        """)
+    parser.add_argument('--local',
+        default=False, action='store_const', const=True,
+        help="""
+        Create and run a container locally. This is equivalent to running it on
+        GCE (--remote)
+        """)
+    parser.add_argument('--remote',
+        default=False, action='store_const', const=True,
+        help="""
+        Create and run a container remotely on Google Container Engine.
+        This option starts rolling-update immediately after uploading container.
+        """)
     args = parser.parse_args()
 
     container_name = get_container_tag('bonsai_frontend')
-    print("New container %s" % container_name)
-
-    create_containers(container_name)
-    if args.local:
+    if args.fake:
+        run_fake_server()
+    elif args.local:
+        create_containers(container_name)
         deploy_containers_local(container_name)
-    else:
+    elif args.remote:
+        create_containers(container_name)
         deploy_containers_gke(container_name)
+    else:
+        print("One of {--local, --remote, --fake} required. See --help for details.")
