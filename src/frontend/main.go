dce9c45839b966deab459d4d7217fbe013130d36
xanxys <xanxys@gmail.com>
2015-09-14 23:09:03 +0900

    Extracted generic handler part.

--- src/frontend/main.go
+++ src/frontend/main.go
@@ -4,6 +4,7 @@ import (
 	"./api"
 	"fmt"
 	"github.com/golang/protobuf/jsonpb"
+	"github.com/golang/protobuf/proto"
 	"mime"
 	"net/http"
 )
@@ -25,6 +26,45 @@ func (fe *FeServiceImpl) HandleWorlds(q *api.WorldsQ) *api.WorldsS {
 				NumTicks: &nTicks,
 			},
 		},
+	}
+}
+
+// MaybeExtractQ extracts proto from HTTP request and returns it.
+// Nil indicates failure, and appropriate status / description is written
+// to response.
+func MaybeExtractQ(w http.ResponseWriter, r *http.Request, defaultQ proto.Message) *proto.Message {
+	q := proto.Clone(defaultQ)
+	err := r.ParseForm()
+	if err != nil {
+		http.NotFound(w, r)
+		fmt.Fprintf(w, "strange query %v", err)
+		return nil
+	}
+	pb := r.Form.Get("pb")
+	if pb == "" {
+		http.NotFound(w, r)
+		fmt.Fprintf(w, "pb param required")
+		return nil
+	}
+	err = jsonpb.UnmarshalString(pb, q)
+	if err != nil {
+		fmt.Fprintf(w, "Failed to parse pb param %v", err)
+		return nil
+	}
+	return &q
+}
+
+func WriteS(
+	w http.ResponseWriter, r *http.Request, s proto.Message) {
+
+	w.Header().Set("Content-Type", "application/json")
+	marshaler := jsonpb.Marshaler{
+		EnumsAsString: true,
+		Indent:        "",
+	}
+	err := marshaler.Marshal(w, s)
+	if err != nil {
+		fmt.Fprintf(w, "{\"error\": \"Internal error: failed to encode return pb\"}")
 	}
 }
 
@@ -52,34 +92,10 @@ func main() {
 	fe := &FeServiceImpl{}
 
 	http.HandleFunc("/api/worlds", func(w http.ResponseWriter, r *http.Request) {
-		q := api.WorldsQ{}
-		err := r.ParseForm()
-		if err != nil {
-			http.NotFound(w, r)
-			fmt.Fprintf(w, "strange query %v", err)
-			return
-		}
-		pb := r.Form.Get("pb")
-		if pb == "" {
-			http.NotFound(w, r)
-			fmt.Fprintf(w, "pb param required")
-			return
-		}
-		err = jsonpb.UnmarshalString(pb, &q)
-		if err != nil {
-			fmt.Fprintf(w, "Failed to parse pb param %v", err)
-			return
-		}
-		s := fe.HandleWorlds(&q)
-
-		w.Header().Set("Content-Type", "application/json")
-		marshaler := jsonpb.Marshaler{
-			EnumsAsString: true,
-			Indent:        "",
-		}
-		err = marshaler.Marshal(w, s)
-		if err != nil {
-			fmt.Fprintf(w, "{\"error\": \"Internal error: failed to encode return pb\"}")
+		q := MaybeExtractQ(w, r, &api.WorldsQ{})
+		if q != nil {
+			s := fe.HandleWorlds((*q).(*api.WorldsQ))
+			WriteS(w, r, s)
 		}
 	})
 
