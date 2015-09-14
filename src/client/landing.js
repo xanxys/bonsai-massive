7be864275381fa538b6d2936c4287a76de2739c5
xanxys <xanxys@gmail.com>
2015-09-15 00:00:19 +0900

    Remove default biospheres from landing page.

--- src/client/landing.js
+++ src/client/landing.js
@@ -4,21 +4,10 @@ $(document).ready(() => {
     var bs = new Vue({
         el: '#biospheres',
         data: {
-            biospheres: [
-                {
-                    name: "Test World",
-                    num_cores: 123,
-                    num_ticks: 23456
-                },
-                {
-                    name: "Big World 2",
-                    num_cores: 45,
-                    num_ticks: 3232132
-                }
-            ]
+            biospheres: []
         },
         methods: {
-            update: function() {
+            update: () => {
                 var biospheres = this.biospheres;
                 $.ajax('/api/biospheres', {
                     "data": {
@@ -30,4 +19,5 @@ $(document).ready(() => {
             }
         }
     });
+    bs.update();
 });
