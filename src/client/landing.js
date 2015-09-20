2f9f2bdaf88b9199a041fdd2dfd0790d86081a0a
xanxys <xanxys@gmail.com>
2015-09-20 15:19:20 +0900

    Adjust dialog text.

--- src/client/landing.js
+++ src/client/landing.js
@@ -24,6 +24,19 @@ $(document).ready(() => {
     $('#create_biosphere').click(() => {
         $('#create_biosphere_dialog').show();
 
+        var bs = new Vue({
+            el: '#create_biosphere_dialog',
+            data: {
+                name: ""
+            },
+            computed: {
+                // For some reason, when I write ""() => ..." here, vue.js
+                // fails to detect dependency and do not auto-update.
+                create_ready: function() {
+                    return this.name != '';
+                }
+            }
+        });
     })
 
     $('#create_biosphere_yes').click(() => {
