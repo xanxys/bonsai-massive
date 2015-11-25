d13e3b5cb4fc0871168d952c7b46b943b73a1dc4
xanxys <xanxys@gmail.com>
2015-11-25 22:17:55 +0900

    Add dark connection to chunk server from biosphere_frames.

--- src/frontend/handle_biosphere_frames.go
+++ src/frontend/handle_biosphere_frames.go
@@ -2,16 +2,43 @@ package main
 
 import (
 	"./api"
+	"fmt"
 	"golang.org/x/net/context"
+	"google.golang.org/grpc"
 	"log"
 	"math"
+	"time"
 )
 
 func (fe *FeServiceImpl) BiosphereFrames(ctx context.Context, q *api.BiosphereFramesQ) (*api.BiosphereFramesS, error) {
-	polysoup := icosahedron(NewVec3f0(), 0.1).Serialize()
-	return &api.BiosphereFramesS{
-		Content: polysoup,
-	}, nil
+	chunks, err := fe.GetChunkServerInstances(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(chunks) == 0 {
+		log.Print("Failed to connect to a chunk server; returning dummy frame")
+		polysoup := icosahedron(NewVec3f0(), 0.1).Serialize()
+		return &api.BiosphereFramesS{
+			Content: polysoup,
+		}, nil
+	} else {
+		chunkInstance := chunks[0]
+		ip := chunkInstance.NetworkInterfaces[0].NetworkIP
+
+		conn, err := grpc.Dial(fmt.Sprintf("%s:9000", ip),
+			grpc.WithInsecure(), grpc.WithBlock(), grpc.WithTimeout(100*time.Millisecond))
+		if err != nil {
+			return nil, err
+		}
+		defer conn.Close()
+		chunkService := api.NewChunkServiceClient(conn)
+		log.Printf("Connected to chunk server %v, but returning dummy because chunk is not implemented yet", chunkService)
+		polysoup := icosahedron(NewVec3f0(), 0.1).Serialize()
+		return &api.BiosphereFramesS{
+			Content: polysoup,
+		}, nil
+	}
 }
 
 type Mesh []Vertex
