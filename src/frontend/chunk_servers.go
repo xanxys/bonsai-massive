1392202222da7eb01782ac3d71b7cdb561bd952b
xanxys <xanxys@gmail.com>
2015-11-25 22:11:41 +0900

    Refactor chunk server enumeration.

---
+++ src/frontend/chunk_servers.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"golang.org/x/net/context"
+	"google.golang.org/api/compute/v1"
+)
+
+// GetChunkServerInstances returns GCE instance tagged as chunk servers.
+// It does not guarantee avaiability of them.
+func (fe *FeServiceImpl) GetChunkServerInstances(ctx context.Context) ([]*compute.Instance, error) {
+	service, err := fe.authCompute(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	list, err := service.Instances.List(ProjectId, zone).Do()
+	if err != nil {
+		return nil, err
+	}
+
+	var chunkServers []*compute.Instance
+	for _, instance := range list.Items {
+		metadata := make(map[string]string)
+		for _, item := range instance.Metadata.Items {
+			metadata[item.Key] = *item.Value
+		}
+		ty, ok := metadata["bonsai-type"]
+		if ok && ty == "chunk" {
+			chunkServers = append(chunkServers, instance)
+		}
+	}
+	return chunkServers, nil
+}
--- src/frontend/handle_debug.go
+++ src/frontend/handle_debug.go
@@ -8,55 +8,40 @@ import (
 	"time"
 )
 
-// HandleDebug translate as much errors into human-readable errors instead
+// Debug translate as much errors into human-readable errors instead
 // of logging, unlike other handles.
 func (fe *FeServiceImpl) Debug(ctx context.Context, q *api.DebugQ) (*api.DebugS, error) {
-	service, err := fe.authCompute(ctx)
+	chunkInstances, err := fe.GetChunkServerInstances(ctx)
 	if err != nil {
 		return &api.DebugS{
-			ChunkServersError: fmt.Sprintf("Failed to get GCE access: %v", err),
-		}, nil
-	}
-
-	list, err := service.Instances.List(ProjectId, zone).Do()
-	if err != nil {
-		return &api.DebugS{
-			ChunkServersError: fmt.Sprintf("Failed to get GCE instance list: %#v", err),
+			ChunkServersError: fmt.Sprintf("Failed to list chunk servers: %v", err),
 		}, nil
 	}
 
 	var chunkServers []*api.DebugS_ChunkServerState
-	for _, instance := range list.Items {
-		metadata := make(map[string]string)
-		for _, item := range instance.Metadata.Items {
-			metadata[item.Key] = *item.Value
+	for _, instance := range chunkInstances {
+		ip := instance.NetworkInterfaces[0].NetworkIP
+		serverState := api.DebugS_ChunkServerState{
+			IpAddress: ip,
+			Health:    api.DebugS_ALLOCATED,
 		}
-		ty, ok := metadata["bonsai-type"]
-		if ok && ty == "chunk" {
-			ip := instance.NetworkInterfaces[0].NetworkIP
-			serverState := api.DebugS_ChunkServerState{
-				IpAddress: ip,
-				Health:    api.DebugS_ALLOCATED,
-			}
-			conn, err := grpc.Dial(fmt.Sprintf("%s:9000", ip),
-				grpc.WithInsecure(), grpc.WithBlock(), grpc.WithTimeout(100*time.Millisecond))
-			if conn != nil {
-				defer conn.Close()
-				serverState.Health = api.DebugS_GRPC_OK
-				chunkService := api.NewChunkServiceClient(conn)
-				resp, err := chunkService.Status(ctx, &api.StatusQ{})
-				if err != nil {
-					serverState.State = fmt.Sprintf("%v", err)
-				} else {
-					serverState.Health = api.DebugS_STATUS_OK
-					serverState.State = fmt.Sprintf("%v", resp)
-				}
-			} else {
+		conn, err := grpc.Dial(fmt.Sprintf("%s:9000", ip),
+			grpc.WithInsecure(), grpc.WithBlock(), grpc.WithTimeout(100*time.Millisecond))
+		if conn != nil {
+			defer conn.Close()
+			serverState.Health = api.DebugS_GRPC_OK
+			chunkService := api.NewChunkServiceClient(conn)
+			resp, err := chunkService.Status(ctx, &api.StatusQ{})
+			if err != nil {
 				serverState.State = fmt.Sprintf("%v", err)
+			} else {
+				serverState.Health = api.DebugS_STATUS_OK
+				serverState.State = fmt.Sprintf("%v", resp)
 			}
-			chunkServers = append(chunkServers, &serverState)
+		} else {
+			serverState.State = fmt.Sprintf("%v", err)
 		}
-
+		chunkServers = append(chunkServers, &serverState)
 	}
 	return &api.DebugS{
 		ChunkServers: chunkServers,
