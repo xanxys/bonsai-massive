369a9ba4cf2bad718eca419b616d4c39ec2ec547
xanxys <xanxys@gmail.com>
2015-11-25 21:36:27 +0900

    Create a wrapper for manipulating mesh.

--- src/frontend/handle_biosphere_frames.go
+++ src/frontend/handle_biosphere_frames.go
@@ -3,10 +3,47 @@ package main
 import (
 	"./api"
 	"golang.org/x/net/context"
+	"log"
 	"math"
 )
 
 func (fe *FeServiceImpl) BiosphereFrames(ctx context.Context, q *api.BiosphereFramesQ) (*api.BiosphereFramesS, error) {
+	polysoup := icosahedron(NewVec3f0(), 0.1).Serialize()
+	return &api.BiosphereFramesS{
+		Content: polysoup,
+	}, nil
+}
+
+type Mesh []Vertex
+
+func (mesh Mesh) Serialize() *api.PolySoup {
+	if len(mesh)%3 != 0 {
+		log.Panicf("Trying to serialize broken mesh with #vertices=%d", len(mesh))
+	}
+
+	ps := api.PolySoup{
+		Vertices: make([]*api.PolySoup_Vertex, len(mesh)),
+	}
+	for ix, vert := range mesh {
+		ps.Vertices[ix] = &api.PolySoup_Vertex{
+			Px: vert.Pos.X,
+			Py: vert.Pos.Y,
+			Pz: vert.Pos.Z,
+		}
+	}
+	return &ps
+}
+
+// Vertex is a wrapper for PolySoup_Vertex using Vector.
+type Vertex struct {
+	Pos Vec3f
+	Nr  Vec3f
+	Col Vec3f
+}
+
+// Create an icosahedron mesh. returned mesh will be approximation of
+// a sphere of given center and radius, but radius is actually ill-defined.
+func icosahedron(center Vec3f, radius float32) Mesh {
 	// Icosahedron definition.
 	// Adopted from https://github.com/mrdoob/three.js/blob/master/src/extras/geometries/IcosahedronGeometry.js
 	t := float32((1 + math.Sqrt(5)) / 2)
@@ -22,16 +59,12 @@ func (fe *FeServiceImpl) BiosphereFrames(ctx context.Context, q *api.BiosphereFr
 		4, 9, 5, 2, 4, 11, 6, 2, 10, 8, 6, 7, 9, 8, 1,
 	}
 
-	ps := &api.PolySoup{}
+	var vs Mesh
 	for i := 0; i < len(indices); i++ {
-		ps.Vertices = append(ps.Vertices, &api.PolySoup_Vertex{
-			Px: vertices[indices[i]*3+0],
-			Py: vertices[indices[i]*3+1],
-			Pz: vertices[indices[i]*3+2],
+		posBase := Vec3f{vertices[indices[i]*3+0], vertices[indices[i]*3+1], vertices[indices[i]*3+2]}
+		vs = append(vs, Vertex{
+			Pos: posBase.MultS(radius * 0.5).Add(center),
 		})
 	}
-
-	return &api.BiosphereFramesS{
-		Content: ps,
-	}, nil
+	return vs
 }
