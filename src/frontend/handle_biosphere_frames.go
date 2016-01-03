8218a749c153c5148a61262e130a3040f10dfb1c
xanxys <xanxys@gmail.com>
2016-01-03 20:00:57 +0900

    Put back real biosphere_frames handler.

--- src/frontend/handle_biosphere_frames.go
+++ src/frontend/handle_biosphere_frames.go
@@ -2,78 +2,79 @@ package main
 
 import (
 	"./api"
-	//"errors"
-	//"fmt"
+	"errors"
+	"fmt"
 	"golang.org/x/net/context"
 	"google.golang.org/cloud/datastore"
-	//"google.golang.org/grpc"
+	"google.golang.org/grpc"
 	"log"
 	"math/rand"
-	//"time"
+	"time"
 )
 
 func (fe *FeServiceImpl) BiosphereFrames(ctx context.Context, q *api.BiosphereFramesQ) (*api.BiosphereFramesS, error) {
-	var mesh Mesh
-	for ix := 0; ix < 10000; ix++ {
-		pos := Vec3f{rand.Float32(), rand.Float32(), rand.Float32()}
-		grainMesh := Icosahedron(pos, 0.06)
-		baseColor := Vec3f{0.5, 0.5, 0.5}
-		grainMesh.SetColor(baseColor.Add(Vec3f{rand.Float32(), rand.Float32(), rand.Float32()}.MultS(0.2)))
-		mesh = append(mesh, grainMesh...)
-	}
-
-	return &api.BiosphereFramesS{
-		Content:          mesh.Serialize(),
-		ContentTimestamp: 0,
-	}, nil
-
 	/*
-		chunks, err := fe.GetChunkServerInstances(ctx)
-		if err != nil {
-			return nil, err
+		var mesh Mesh
+		for ix := 0; ix < 10000; ix++ {
+			pos := Vec3f{rand.Float32(), rand.Float32(), rand.Float32()}
+			grainMesh := Icosahedron(pos, 0.06)
+			baseColor := Vec3f{0.5, 0.5, 0.5}
+			grainMesh.SetColor(baseColor.Add(Vec3f{rand.Float32(), rand.Float32(), rand.Float32()}.MultS(0.2)))
+			mesh = append(mesh, grainMesh...)
 		}
 
-		bsTopo, err := fe.getBiosphereTopo(ctx, q.BiosphereId)
-		if err != nil {
-			return nil, err
-		}
-
-		if len(chunks) == 0 {
-			log.Print("Active chunk server not found, returning dummy frame.")
-			return &api.BiosphereFramesS{
-				Content: fallbackContent(),
-			}, nil
-		}
-
-		chunkInstance := chunks[0]
-		ip := chunkInstance.NetworkInterfaces[0].NetworkIP
-
-		conn, err := grpc.Dial(fmt.Sprintf("%s:9000", ip),
-			grpc.WithInsecure(), grpc.WithBlock(), grpc.WithTimeout(100*time.Millisecond))
-		if err != nil {
-			return nil, err
-		}
-		defer conn.Close()
-		chunkService := api.NewChunkServiceClient(conn)
-		chunkIds := make([]string, len(bsTopo.GetChunkTopos()))
-		for ix, chunkTopo := range bsTopo.GetChunkTopos() {
-			chunkIds[ix] = chunkTopo.ChunkId
-		}
-		resp, err := chunkService.Snapshot(ctx, &api.SnapshotQ{
-			ChunkId: chunkIds,
-		})
-		if err != nil {
-			log.Printf("ChunkService.Snapshot failed %v", err)
-			return nil, err
-		}
-		if resp.Snapshot == nil {
-			return nil, errors.New("ChunkServer.Snapshot doesn't contain snapshot")
-		}
 		return &api.BiosphereFramesS{
-			Content:          snapshotToMesh(bsTopo, resp.Snapshot).Serialize(),
-			ContentTimestamp: resp.Timestamp,
+			Content:          mesh.Serialize(),
+			ContentTimestamp: 0,
 		}, nil
 	*/
+
+	chunks, err := fe.GetChunkServerInstances(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	bsTopo, err := fe.getBiosphereTopo(ctx, q.BiosphereId)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(chunks) == 0 {
+		log.Print("Active chunk server not found, returning dummy frame.")
+		return &api.BiosphereFramesS{
+			Content: fallbackContent(),
+		}, nil
+	}
+
+	chunkInstance := chunks[0]
+	ip := chunkInstance.NetworkInterfaces[0].NetworkIP
+
+	conn, err := grpc.Dial(fmt.Sprintf("%s:9000", ip),
+		grpc.WithInsecure(), grpc.WithBlock(), grpc.WithTimeout(100*time.Millisecond))
+	if err != nil {
+		return nil, err
+	}
+	defer conn.Close()
+	chunkService := api.NewChunkServiceClient(conn)
+	chunkIds := make([]string, len(bsTopo.GetChunkTopos()))
+	for ix, chunkTopo := range bsTopo.GetChunkTopos() {
+		chunkIds[ix] = chunkTopo.ChunkId
+	}
+	resp, err := chunkService.Snapshot(ctx, &api.SnapshotQ{
+		ChunkId: chunkIds,
+	})
+	if err != nil {
+		log.Printf("ChunkService.Snapshot failed %v", err)
+		return nil, err
+	}
+	if resp.Snapshot == nil {
+		return nil, errors.New("ChunkServer.Snapshot doesn't contain snapshot")
+	}
+	return &api.BiosphereFramesS{
+		Content:          snapshotToMesh(bsTopo, resp.Snapshot).Serialize(),
+		ContentTimestamp: resp.Timestamp,
+	}, nil
+
 }
 
 func (fe *FeServiceImpl) getBiosphereTopo(ctx context.Context, biosphereId uint64) (BiosphereTopology, error) {
