50f71a282c383ad27da3ba5bc730043f9c2faf71
xanxys <xanxys@gmail.com>
2016-01-10 16:56:30 +0900

    Cleanup startup-script based alloc code.

--- src/frontend/chunk.go
+++ src/frontend/chunk.go
@@ -5,7 +5,6 @@ import (
 	"google.golang.org/api/compute/v1"
 	"log"
 	"math/rand"
-	//"strings"
 )
 
 const GoogleCloudLoggingScope = "https://www.googleapis.com/auth/logging.write"
@@ -18,25 +17,6 @@ func (fe *FeServiceImpl) prepare(service *compute.Service) {
 	// Run `gcloud compute images list --project google-containers`
 	// to see list of container names.
 	imageURL := "https://www.googleapis.com/compute/v1/projects/google-containers/global/images/container-vm-v20151215"
-
-	// TODO: migrate to kubelet manifest file.
-	/*
-		startupScript := strings.Join(
-			[]string{
-				`#!/bin/bash`,
-				`METADATA=http://metadata.google.internal./computeMetadata/v1`,
-				`SVC_ACCT=$METADATA/instance/service-accounts/default`,
-				`ACCESS_TOKEN=$(curl -H 'Metadata-Flavor: Google' $SVC_ACCT/token | cut -d'"' -f 4)`,
-				`docker login -e dummy@example.com -u _token -p $ACCESS_TOKEN https://gcr.io`,
-				`curl -sSO https://dl.google.com/cloudagents/install-logging-agent.sh`,
-				`sha256sum install-logging-agent.sh`,
-				`bash install-logging-agent.sh`,
-				`printf '<source>\ntype forward \nport 24224\n</source>\n' > /etc/google-fluentd/config.d/docker.conf`,
-				`service google-fluentd restart`,
-				fmt.Sprintf(`docker pull %s`, fe.chunkContainerName),
-				fmt.Sprintf(`docker run -d --log-driver=fluentd --publish 9000:9000 %s`, fe.chunkContainerName),
-			}, "\n")
-	*/
 
 	containerManifest := fmt.Sprintf(`
 		{
@@ -128,12 +108,6 @@ func (fe *FeServiceImpl) prepare(service *compute.Service) {
 		},
 		Metadata: &compute.Metadata{
 			Items: []*compute.MetadataItems{
-				/*
-					{
-						Key:   "startup-script",
-						Value: &startupScript,
-					},
-				*/
 				{
 					Key:   "bonsai-type",
 					Value: &bonsaiType,
