984dacefffff5100bde1f6e699f53a7fe0482db2
xanxys <xanxys@gmail.com>
2016-05-07 18:35:42 +0900

    Delete timing visualizer from debug page since it's migrated to trace viewer in GCP.

--- src/client/debug.js
+++ src/client/debug.js
@@ -21,28 +21,4 @@ $(document).ready(() => {
         }
     });
     bs.update();
-
-    let bs_timing = new Vue({
-        el: '#trace_visualizer',
-        data: {
-        },
-        methods: {
-            refresh: function() {
-                let trace = JSON.parse($('#trace_json').val());
-                $('#timing').empty();
-
-                let t0 = trace.start;
-                let px_per_ns = 500 / (trace.end - trace.start);
-                let put = (tr) => {
-                    let bar = $('<div/>').css('margin-bottom', '4px');
-                    let time = ((tr.end - tr.start) * 1e-9).toFixed(3) + 's';
-                    bar.append($('<span/>').css('display', 'inline-block').css('width', (tr.start - t0) * px_per_ns + 'px'));
-                    bar.append($('<span/>').css('display', 'inline-block').css('width', (tr.end - tr.start) * px_per_ns + 'px').css('background-color', '#B2DFDB').css('height', '24px').text(tr.name + ': ' + time));
-                    $('#timing').append(bar);
-                    _.each(tr.children, put);
-                };
-                put(trace);
-            },
-        },
-    });
 });
