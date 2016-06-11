d0ba54dfdd9604af94800e9fe67351271119e8d8
xanxys <xanxys@gmail.com>
2016-06-11 18:01:36 +0900

    Make timestamp adjustable.

--- src/client/debug.js
+++ src/client/debug.js
@@ -57,7 +57,9 @@ $(document).ready(() => {
     let bs_stepping = new Vue({
         el: '#card_stepping',
         data: {
-            response: ""
+            response: "",
+            stepping_min_date_str: "",
+            stepping_max_date_str: "",
         },
         methods: {
             // For some reason, () => doesn't work.
@@ -85,44 +87,70 @@ $(document).ready(() => {
                     machine_ip,
                     chunk_id
                 )`;
+                let vm = this;
 
                 authAndCallBq(query).then((resp) => {
                     let container = document.getElementById('stepping_timeline');
-                    let chart = new google.visualization.Timeline(container);
-                    let dataTable = new google.visualization.DataTable();
+                    bs_stepping.chart = new google.visualization.Timeline(container);
+                    vm.stepping_rows = resp.result.rows;
 
-                    dataTable.addColumn({
-                        type: 'string',
-                        id: 'Location'
-                    });
-                    dataTable.addColumn({
-                        type: 'string',
-                        id: 'Event'
-                    });
-                    dataTable.addColumn({
-                        type: 'date',
-                        id: 'Start'
-                    });
-                    dataTable.addColumn({
-                        type: 'date',
-                        id: 'End'
-                    });
+                    var min_time_ms = 1e20;
+                    var max_time_ms = 0;
                     _.each(resp.result.rows, (row_location) => {
-                        let location = row_location.f[0].v + "/" + row_location.f[1].v;
-
                         _.each(row_location.f[2].v, (row_ev) => {
                             let timestamp = parseInt(row_ev.v.f[0].v);
-                            let ev = row_ev.v.f[1].v;
-                            dataTable.addRow(
-                                [location, ev, new Date(timestamp), new Date(timestamp + 100)]
-                            );
+                            min_time_ms = Math.min(min_time_ms, timestamp);
+                            max_time_ms = Math.max(max_time_ms, timestamp);
                         });
-                    })
-                    chart.draw(dataTable);
+                    });
+                    vm.stepping_min_date_str = new Date(min_time_ms).toISOString();
+                    vm.stepping_max_date_str = new Date(max_time_ms).toISOString();
                 }, (fail) => {
                     console.log('Failed to do BQ');
                 });
             }
         }
     });
+
+    let maybe_update_range = () => {
+        let min_d = new Date(bs_stepping.stepping_min_date_str);
+        let max_d = new Date(bs_stepping.stepping_max_date_str);
+        if (isNaN(min_d) || isNaN(max_d)) {
+            return;
+        }
+
+        let dataTable = new google.visualization.DataTable();
+        dataTable.addColumn({
+            type: 'string',
+            id: 'Location'
+        });
+        dataTable.addColumn({
+            type: 'string',
+            id: 'Event'
+        });
+        dataTable.addColumn({
+            type: 'date',
+            id: 'Start'
+        });
+        dataTable.addColumn({
+            type: 'date',
+            id: 'End'
+        });
+
+        _.each(bs_stepping.stepping_rows, (row_location) => {
+            let location = row_location.f[0].v + "/" + row_location.f[1].v;
+            _.each(row_location.f[2].v, (row_ev) => {
+                let timestamp = parseInt(row_ev.v.f[0].v);
+                if (new Date(timestamp) < min_d || max_d < new Date(timestamp)) {
+                    return;
+                }
+                let ev = row_ev.v.f[1].v;
+                dataTable.addRow([location, ev, new Date(timestamp), new Date(timestamp + 100)]);
+            });
+        });
+        // Setting viewWindow doesn't work in timeline chart.
+        bs_stepping.chart.draw(dataTable, {height: 600});
+    };
+    bs_stepping.$watch('stepping_min_date_str', maybe_update_range);
+    bs_stepping.$watch('stepping_max_date_str', maybe_update_range);
 });
