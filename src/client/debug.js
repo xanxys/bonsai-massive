7c014f5baeaa75979abf08647d8b0bca0ea8850e
xanxys <xanxys@gmail.com>
2016-06-28 23:08:14 +0900

    Implement window-restricted query for visualizer to avoid response size limit.

--- src/client/debug.js
+++ src/client/debug.js
@@ -66,6 +66,7 @@ $(document).ready(() => {
             window_max: 0,
             view_min_ratio: 0,
             view_max_ratio: 10000,
+            loading_detail: false,
         },
         computed: {
             view_min: function() {
@@ -111,6 +112,34 @@ $(document).ready(() => {
                 group by time_bucket
                 order by time_bucket
                 `;
+
+                let vm = this;
+
+                authAndCallBq(query_summary).then((resp) => {
+                    let data = new google.visualization.DataTable();
+                    data.addColumn('datetime', 'bucket');
+                    data.addColumn('number', '#events');
+                    let ts = [];
+                    _.each(resp.result.rows, (row) => {
+                        let t = parseFloat(row.f[0].v);
+                        data.addRow([new Date(t), parseInt(row.f[1].v)]);
+                        ts.push(t);
+                    });
+                    let chart = new google.visualization.Table(document.getElementById('stepping_summary'));
+                    chart.draw(data);
+                    google.visualization.events.addListener(chart, 'select', (ev) => {
+                        let selected_ts = _.map(chart.getSelection(), (sel) => ts[sel.row]);
+                        vm.select_bucket(selected_ts);
+                    });
+                });
+            },
+            select_bucket: function(selection) {
+                let vm = this;
+                vm.view_min_ratio = 0;
+                vm.view_max_ratio = 10000;
+                vm.window_min = _.min(selection);
+                vm.window_max = _.max(selection) + 3600e3; // one bucket + 1hr
+
                 let query = `select
                   machine_ip,
                   chunk_id,
@@ -132,108 +161,84 @@ $(document).ready(() => {
                     ) as events
                   from
                     \`platform.stepping\`
+                  where ${vm.window_min} <= unix_millis(start_at) and unix_millis(start_at) <= ${vm.window_max}
                   group by
                     machine_ip,
                     chunk_id
                 )`;
-                let vm = this;
-
-                authAndCallBq(query_summary).then((resp) => {
-                    let data = new google.visualization.DataTable();
-                    data.addColumn('datetime', 'bucket');
-                    data.addColumn('number', '#events');
-                    let ts = [];
-                    _.each(resp.result.rows, (row) => {
-                        let t = parseFloat(row.f[0].v);
-                        data.addRow([new Date(t), parseInt(row.f[1].v)]);
-                        ts.push(t);
-                    });
-                    let chart = new google.visualization.Table(document.getElementById('stepping_summary'));
-                    chart.draw(data);
-                    google.visualization.events.addListener(chart, 'select', (ev) => {
-                        let selected_ts = _.map(chart.getSelection(), (sel) => ts[sel.row]);
-                        vm.select_bucket(selected_ts);
-                    });
-                });
-
+                this.loading_detail = true;
                 authAndCallBq(query).then((resp) => {
                     bs_stepping.chart = new google.visualization.Timeline(document.getElementById('stepping_timeline'));
                     vm.stepping_rows = resp.result.rows;
+                    vm.loading_detail = false;
+                    vm.maybe_update_range();
                 });
             },
-            select_bucket: function(selection) {
-                console.log(selection);
-                this.window_min = _.min(selection);
-                this.window_max = _.max(selection) + 3600e3; // one bucket + 1hr
-                this.view_min_ratio = 0;
-                this.view_max_ratio = 10000;
+            maybe_update_range:  function() {
+                if (this.chart === undefined) {
+                    return;
+                }
+                let min_d = this.view_min;
+                let max_d = this.view_max;
+                if (isNaN(new Date(min_d)) || isNaN(new Date(max_d))) {
+                    return;
+                }
+                let dataTable = new google.visualization.DataTable();
+                dataTable.addColumn({
+                    type: 'string',
+                    id: 'Location'
+                });
+                dataTable.addColumn({
+                    type: 'string',
+                    id: 'Event'
+                });
+                dataTable.addColumn({
+                    type: 'string',
+                    id: 'EventFull',
+                    role: 'tooltip'
+                });
+                dataTable.addColumn({
+                    type: 'date',
+                    id: 'Start'
+                });
+                dataTable.addColumn({
+                    type: 'date',
+                    id: 'End'
+                });
+                let rows = [];
+                _.each(this.stepping_rows, (row_location) => {
+                    let location = row_location.f[0].v + "/" + row_location.f[1].v;
+                    _.each(row_location.f[2].v, (row_ev) => {
+                        let timestamp_start = parseInt(row_ev.v.f[0].v);
+                        let timestamp_end = parseInt(row_ev.v.f[1].v);
+                        // Don't show if there's no overlap between event span & current view.
+                        if (timestamp_end < min_d || max_d < timestamp_start) {
+                            return;
+                        }
+                        timestamp_start = Math.max(timestamp_start, min_d);
+                        timestamp_end = Math.min(timestamp_end, max_d);
+
+                        let ev_type = row_ev.v.f[2].v;
+                        let ev_label = `${ev_type} (${row_ev.v.f[3].v})`;
+                        rows.push([location, ev_type, ev_label, new Date(timestamp_start), new Date(timestamp_end)]);
+                    });
+                });
+                const max_num_rows = 400;
+                if (rows.length > max_num_rows) {
+                    console.log('Warning: some rows are not shown because of threshold', max_num_rows);
+                }
+                dataTable.addRows(rows.slice(0, max_num_rows));
+                // Setting viewWindow doesn't work in timeline chart.
+                this.chart.draw(dataTable, {
+                    height: 600,
+                    hAxis: {
+                        minValue: new Date(min_d),
+                        maxValue: new Date(max_d)
+                    }
+                });
             },
         }
     });
-
-    let maybe_update_range = () => {
-        if (bs_stepping.chart === undefined) {
-            return;
-        }
-        let min_d = bs_stepping.view_min;
-        let max_d = bs_stepping.view_max;
-        if (isNaN(new Date(min_d)) || isNaN(new Date(max_d))) {
-            return;
-        }
-        let dataTable = new google.visualization.DataTable();
-        dataTable.addColumn({
-            type: 'string',
-            id: 'Location'
-        });
-        dataTable.addColumn({
-            type: 'string',
-            id: 'Event'
-        });
-        dataTable.addColumn({
-            type: 'string',
-            id: 'EventFull',
-            role: 'tooltip'
-        });
-        dataTable.addColumn({
-            type: 'date',
-            id: 'Start'
-        });
-        dataTable.addColumn({
-            type: 'date',
-            id: 'End'
-        });
-        let rows = [];
-        _.each(bs_stepping.stepping_rows, (row_location) => {
-            let location = row_location.f[0].v + "/" + row_location.f[1].v;
-            _.each(row_location.f[2].v, (row_ev) => {
-                let timestamp_start = parseInt(row_ev.v.f[0].v);
-                let timestamp_end = parseInt(row_ev.v.f[1].v);
-                // Don't show if there's no overlap between event span & current view.
-                if (timestamp_end < min_d || max_d < timestamp_start) {
-                    return;
-                }
-                timestamp_start = Math.max(timestamp_start, min_d);
-                timestamp_end = Math.min(timestamp_end, max_d);
-
-                let ev_type = row_ev.v.f[2].v;
-                let ev_label = `${ev_type} (${row_ev.v.f[3].v})`;
-                rows.push([location, ev_type, ev_label, new Date(timestamp_start), new Date(timestamp_end)]);
-            });
-        });
-        const max_num_rows = 400;
-        if (rows.length > max_num_rows) {
-            console.log('Warning: some rows are not shown because of threshold', max_num_rows);
-        }
-        dataTable.addRows(rows.slice(0, max_num_rows));
-        // Setting viewWindow doesn't work in timeline chart.
-        bs_stepping.chart.draw(dataTable, {
-            height: 600,
-            hAxis: {
-                minValue: new Date(min_d),
-                maxValue: new Date(max_d)
-            }
-        });
-    };
-    bs_stepping.$watch('view_min', maybe_update_range);
-    bs_stepping.$watch('view_max', maybe_update_range);
+    bs_stepping.$watch('view_min', () => {bs_stepping.maybe_update_range();});
+    bs_stepping.$watch('view_max', () => {bs_stepping.maybe_update_range();});
 });
